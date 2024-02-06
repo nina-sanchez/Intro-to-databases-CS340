5c31a1c31869a8fae1de2ba590c15247dcfec466
regina sanchez <104878059+nina-sanchez@users.noreply.github.com>
2024-02-05 22:35:01 -0800

    Add files via upload

---
+++ activity-m3-5.sql
@@ -0,0 +1,8 @@
+-- date: 02/01/2024
+-- Adapted from code provided in the canvas module
+-- url: https://canvas.oregonstate.edu/courses/1946034/pages/activity-5-sql-queries-of-multiple-tables-joins?module_item_id=23809309
+
+SELECT Products.ProductNumber, Products.ProductName, InvoiceDetails.LineTotal, InvoiceDetails.OrderQty, InvoiceDetails.UnitPrice
+FROM InvoiceDetails
+INNER JOIN Products ON InvoiceDetails.ProductNumber = Products.ProductNumber
+WHERE InvoiceDetails.InvoiceID = 3;
